fbe4aedc05d00a610a300780edfaa338c9defb3a
Zackary Flake <flakez82@students.rowan.edu>
2016-10-14 23:24:04 -0400

    Initial Creation of Login Screen
    I know it's an ugly bg, but it's the start of something

---
+++ app/src/main/java/seniorproject/badger/Login.java
@@ -0,0 +1,14 @@
+package seniorproject.badger;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
