7b51f9a414f2f5ca52241f5a7ac86c3bf11d9092
Zackary Flake <flakez82@students.rowan.edu>
2016-10-23 13:19:14 -0400

    Some more basic versions of activities
    Group Home, Friends List, Badge Description(WIP)

--- app/src/main/java/seniorproject/badger/Badge.java
+++ app/src/main/java/seniorproject/badger/Badge.java
@@ -7,11 +7,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class Groups extends AppCompatActivity {
+public class Badge extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_groups);
+        setContentView(R.layout.activity_badge);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
+
 }
