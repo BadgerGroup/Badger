e3e7021acc9bf5c46b1194f0f670e70d5e84ba8c
dalopez <lopezd0@students.rowan.edu>
2016-10-31 00:25:37 -0400

    Created and designed basic badge selection screen, will be updated in the future. Have also added the badges that I created to the drawable folder

---
+++ app/src/main/java/seniorproject/badger/BadgeScreen.java
@@ -0,0 +1,13 @@
+package seniorproject.badger;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class BadgeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_badgescreen);
+    }
+}
